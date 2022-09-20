d74cafe5a6b80e45ec3c92d94c7955ca2e991604
eduard <eduard@cieep.com>
2022-09-20 13:36:37 +0200

    Initial commit

---
+++ app/src/main/java/com/cieep/a01_ejerciciointroduccinaandroidb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cieep.a01_ejerciciointroduccinaandroidb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
